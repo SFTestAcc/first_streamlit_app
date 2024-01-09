5cf3b5876e539c0bbbe6951764b5088f4b0471cc
SFTestAcc <156074153+SFTestAcc@users.noreply.github.com>
2024-01-09 15:47:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title("Deploying my first app")
+streamlit.title("My Parents new Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
