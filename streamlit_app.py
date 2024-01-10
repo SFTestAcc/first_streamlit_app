6e9aa49888cc2cea9cd129f864e69e3afcbd873d
SFTestAcc <156074153+SFTestAcc@users.noreply.github.com>
2024-01-10 15:21:10 +0530

    Update streamlit_app.py
    Api get information about user entered fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #diplay the table on the page
 streamlit.dataframe(fruits_to_show)
 
+#New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input("Which fruit would you like information about?", "Kiwi")
+streamlit.text("The user entered", fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
