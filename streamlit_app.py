7743c18f5ebe7e0e451e1d9e5ec7908275ba99a9
SFTestAcc <156074153+SFTestAcc@users.noreply.github.com>
2024-01-10 17:43:23 +0530

    Update streamlit_app.py
    removed streamlit stop function

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,8 +54,6 @@ if streamlit.button("Get fruit load list"):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-
 #Allowing the user to add fruit
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
